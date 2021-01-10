cf853b15c795e8bb0c385adcbe129ead4c44385a
Jairo <jairo.jb@gmail.com>
2021-01-10 16:59:03 -0300

    Finish front-web

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -6,7 +6,7 @@ import {ReactComponent as InstagramIcon} from './instagram.svg';
 function Footer() {
     return (
     <footer className="main-footer">
-        App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+        App desenvolvido durante a 2ª ed. do evento <strong>Semana DevSuperior</strong>
         <div className="footer-icons">
             <a href="https://youtube.com/devsuperior" target="_new">
                 <YouTubeIcon />
